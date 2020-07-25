366e11110d073d046f74b7c1d6e41beb5c22b933
fite-or-flite <chrisfite00@gmail.com>
2020-07-25 17:31:43 -0500

    almost had it.  broke something-framelayout replace

--- app/src/main/java/com/example/slicemobileapp2/MainActivity.java
+++ app/src/main/java/com/example/slicemobileapp2/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.slicemobileapp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            main_menu fragment1 = new main_menu();
+            getFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment1)
+                    .commit();
+        }
+
     }
 }
--- app/src/main/java/com/example/slicemobileapp2/main_menu.java
+++ app/src/main/java/com/example/slicemobileapp2/main_menu.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 class main_menu extends Fragment {
 
@@ -16,6 +18,66 @@ class main_menu extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        View v = inflater.inflate(R.layout.fragment_main_menu, container, false);
+
+        Button pizzaButton = (Button) v.findViewById(R.id.pizzaButton);
+        Button pastaButton = (Button) v.findViewById(R.id.pastaButton);
+        Button calzoneButton = (Button) v.findViewById(R.id.calzoneButton);
+        Button saladsButton = (Button) v.findViewById(R.id.saladsButton);
+        Button sidesButton = (Button) v.findViewById(R.id.sidesButton);
+        Button sweetsButton = (Button) v.findViewById(R.id.sweetsButton);
+        Button slicesButton = (Button) v.findViewById(R.id.slicesButton);
+
+        pizzaButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes fragment transaction to pizza page
+                FragmentManager fragmentManager = getFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                pizza fragment = new pizza();
+                fragmentTransaction.add(R.id.fragment_container, fragment);
+                fragmentTransaction.addToBackStack("pizza");
+                fragmentTransaction.commit();
+            }
+        });
+
+        pastaButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes fragment transaction to pizza page
+            }
+        });
+
+        calzoneButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes fragment transaction to pizza page
+            }
+        });
+
+        saladsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes fragment transaction to pizza page
+            }
+        });
+
+        sidesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes fragment transaction to pizza page
+            }
+        });
+
+        sweetsButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes fragment transaction to pizza page
+            }
+        });
+
+        slicesButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes fragment transaction to pizza page
+            }
+        });
+
         return inflater.inflate(R.layout.fragment_main_menu, container, false);
     }
 }
